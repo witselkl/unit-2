c09a86f7b8c27bc92998c749c10502e2ec7fae2a
witselkl <kwitsell@wisc.edu>
2024-10-02 11:17:33 -0600

    added code to top of geojson in hopes that this was what was missing but still have no clue..

--- js/geojsonTutorial.js
+++ js/geojsonTutorial.js
@@ -1,3 +1,12 @@
+var mymap = L.map('map').setView([51.505, -0.09], 13);
+
+//Example 1.1 line 5...add tile layer. Used to load and display tile layers on the map. Most require attribution, which is set under Layer.
+var tileLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+});
+
+tileLayer.addTo(mymap);
+
 var geojsonFeature = {
     "type": "Feature",
     "properties": {
