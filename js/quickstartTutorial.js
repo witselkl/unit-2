f2c32fbaa252b148824966b6a5451cc5c0ca971c
witselkl <kwitsell@wisc.edu>
2024-10-02 00:02:29 -0600

    update quickstart

--- js/quickstartTutorial.js
+++ js/quickstartTutorial.js
@@ -16,14 +16,15 @@ var tileLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
 });
 
-tileLayer.addTo(map);
+tileLayer.addTo(mymap);
 
 var marker = L.marker([51.5, -0.09]).addTo(mymap);
 
 var circle = L.circle([51.508, -0.11], {
     color: 'red',
     fillColor: '#f03',
-    fillOpacity: 0.5,    radius: 500
+    fillOpacity: 0.5,
+    radius: 500
 }).addTo(mymap);
 
 var polygon = L.polygon([
