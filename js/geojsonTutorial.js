17b680e3efdd7fb0d65cedab65b24264d191723e
witselkl <kwitsell@wisc.edu>
2024-10-02 11:59:24 -0600

    added party

--- js/geojsonTutorial.js
+++ js/geojsonTutorial.js
@@ -54,6 +54,43 @@ L.geoJSON(myLines, {
     style: myStyle
 }).addTo(map);
 
+var states = [{
+    "type": "Feature",
+    "properties": {"party": "Republican"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-104.05, 48.99],
+            [-97.22,  48.98],
+            [-96.58,  45.94],
+            [-104.03, 45.94],
+            [-104.05, 48.99]
+        ]]
+    }
+}, {
+    "type": "Feature",
+    "properties": {"party": "Democrat"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-109.05, 41.00],
+            [-102.06, 40.99],
+            [-102.03, 36.99],
+            [-109.04, 36.99],
+            [-109.05, 41.00]
+        ]]
+    }
+}];
+
+L.geoJSON(states, {
+    style: function(feature) {
+        switch (feature.properties.party) {
+            case 'Republican': return {color: "#ff0000"};
+            case 'Democrat':   return {color: "#0000ff"};
+        }
+    }
+}).addTo(map);
+
 //Used to open popups in certain places of the map
 var popup = L.popup()
     .setLatLng([39.75621, -104.99404])
