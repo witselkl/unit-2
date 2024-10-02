a8ccb726b0db3129918a6c36089883a01f1087c8
witselkl <kwitsell@wisc.edu>
2024-10-02 12:02:27 -0600

    update to feature onEachFeature added

--- js/geojsonTutorial.js
+++ js/geojsonTutorial.js
@@ -106,6 +106,30 @@ L.geoJSON(someGeojsonFeature, {
     }
 }).addTo(map);
 
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent);
+    }
+}
+
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+};
+
+L.geoJSON(geojsonFeature, {
+    onEachFeature: onEachFeature
+}).addTo(map);
+
 //Used to open popups in certain places of the map
 var popup = L.popup()
     .setLatLng([39.75621, -104.99404])
