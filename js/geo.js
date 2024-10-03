927ba074263c37da67556510ccd8b5aa398de594
witselkl <kwitsell@wisc.edu>
2024-10-02 19:33:34 -0600

    copy of quickstart

--- js/geo.js
+++ js/geo.js
@@ -7,24 +7,42 @@ var tileLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 
 tileLayer.addTo(mymap);
 
-var marker = L.marker([39.75621, -104.99404]).addTo(map);
+var marker = L.marker([39.75621, -104.99404]).addTo(mymap);
 
-var circle = L.circle([51.508, -0.11], {
+// a class.. A class for drawing circle overlays on a map
+var circle = L.circle([39.75621, -104.99404], {
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5,
     radius: 500
-}).addTo(map);
+}).addTo(mymap);
 
-var geojsonFeature = {
-    "type": "Feature",
-    "properties": {
-        "name": "Coors Field",
-        "amenity": "Baseball Stadium",
-        "popupContent": "This is where the Rockies play!"
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-104.99404, 39.75621]
-    }
-};
+//A class for drawing polygon overlays on a map. 
+var polygon = L.polygon([
+    [39.7509, -140.08],
+    [39.7503, -140.06],
+    [39.7508, -140.047]
+]).addTo(mymap);
+
+marker.bindPopup("<strong>Hello world!</strong><br />I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+
+//Used to open popups in certain places of the map
+var popup = L.popup()
+    .setLatLng([39.75621, -104.99404])
+    .setContent("I am a standalone popup.")
+    .openOn(mymap);
+
+var popup = L.popup();
+
+//event listeners when clicking on the map
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
