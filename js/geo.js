75b5427e9c56f2712edb8942246b31fc48d8c0c1
witselkl <kwitsell@wisc.edu>
2024-10-02 20:08:59 -0600

    rollback to quick

--- js/geo.js
+++ js/geo.js
@@ -9,24 +9,31 @@ tileLayer.addTo(mymap);
 
 var marker = L.marker([39.75621, -104.99404]).addTo(mymap);
 
-var myLines = [{
-    "type": "LineString",
-    "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
-}, {
-    "type": "LineString",
-    "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
-}];
+// var myLines = [{
+//     "type": "LineString",
+//     "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
+// }, {
+//     "type": "LineString",
+//     "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
+// }];
 
-var myStyle = {
-    "color": "#ff7800",
-    "weight": 5,
-    "opacity": 0.65
-};
+// var myStyle = {
+//     "color": "#ff7800",
+//     "weight": 5,
+//     "opacity": 0.65
+// };
 
-L.geoJSON(myLines, {
-    style: myStyle
-}).addTo(map);
+// L.geoJSON(myLines, {
+//     style: myStyle
+// }).addTo(map);
 
+// a class.. A class for drawing circle overlays on a map
+var circle = L.circle([39.75621, -104.99404], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mymap);
 
 marker.bindPopup("<strong>Hello world!</strong><br />I am a popup.").openPopup();
 circle.bindPopup("I am a circle.");
