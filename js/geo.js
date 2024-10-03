deaba4a0d4396c8a8f4575bbc7002a268221d652
witselkl <kwitsell@wisc.edu>
2024-10-02 19:21:01 -0600

    feature

--- js/geo.js
+++ js/geo.js
@@ -6,3 +6,16 @@ var tileLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 });
 
 tileLayer.addTo(mymap);
+
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+};
