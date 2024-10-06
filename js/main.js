aa88fdb8d6b2f34b49f80be7e03571be3397cc84
sounny <sounny@gmail.com>
2024-10-06 20:23:04 +0000

    Refactor Leaflet map initialization and data loading
    Simplify the code for initializing the Leaflet map and loading GeoJSON data. Remove redundant code and improve readability.

--- js/main.js
+++ js/main.js
@@ -1,199 +1,180 @@
+var mymap = L.map('map').setView([39.75621, -104.99404], 13);
 
-    var mymap = L.map('map').setView([39.75621, -104.99404], 13);
+// Example 1.1 line 5...add tile layer. Used to load and display tile layers on the map. Most require attribution, which is set under Layer.
+var tileLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+});
 
-    //Example 1.1 line 5...add tile layer. Used to load and display tile layers on the map. Most require attribution, which is set under Layer.
-    var tileLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-    });
+tileLayer.addTo(mymap);
 
-    tileLayer.addTo(mymap);
+var marker = L.marker([39.75621, -104.99404]).addTo(mymap);
 
-    var marker = L.marker([39.75621, -104.99404]).addTo(mymap);
+// Load the data
+fetch("data/MegaCities.geojson")
+.then(function(response){
+    return response.json();
+})
+.then(function(json){
+    // Create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(json).addTo(mymap);
+})
 
-    //load the data
-    fetch("data/MegaCities.geojson")
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(json){
-        //create a Leaflet GeoJSON layer and add it to the map
-        L.geoJson(json).addTo(map);
-    })
-
-    /* Map of GeoJSON data from MegaCities.geojson */
-    //declare map var in global scope
-    var map;
-    //function to instantiate the Leaflet map
-    function createMap(){
-        //create the map
-        map = L.map('map', {
-            center: [20, 0],
-            zoom: 2
-        });
-
-        //add OSM base tilelayer
-        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-        }).addTo(map);
-
-        //call getData function
-        getData();
-    };
-
-    function onEachFeature(feature, layer) {
-        //no property named popupContent; instead, create html string with all properties
-        var popupContent = "";
-        if (feature.properties) {
-            //loop to add feature property names and values to html string
-            for (var property in feature.properties){
-                popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
-            }
-            layer.bindPopup(popupContent);
-        };
-    };
-
-
-    //function to retrieve the data and place it on the map
-    function getData(){
-        //load the data
-        fetch("data/MegaCities.geojson")
-            .then(function(response){
-                return response.json();
-            })
-            .then(function(json){
-                //create a Leaflet GeoJSON layer and add it to the map
-                L.geoJson(json).addTo(map);
-            })
-    };
-
-    document.addEventListener('DOMContentLoaded',createMap)
-
-    // function onEachFeature(feature, layer) {
-    //     // does this feature have a property named popupContent?
-    //     if (feature.properties && feature.properties.popupContent) {
-    //         layer.bindPopup(feature.properties.popupContent);
-    //     }
-    // }
-
-    var geojsonFeature = {
-        "type": "Feature",
-        "properties": {
-            "name": "Coors Field",
-            "amenity": "Baseball Stadium",
-            "popupContent": "This is where the Rockies play!"
-        },
-        "geometry": {
-            "type": "Point",
-            "coordinates": [-104.99404, 39.75621]
-        }
-    };
-
-    L.geoJSON(geojsonFeature, {
-        onEachFeature: onEachFeature
-    }).addTo(map);
-
-    var myLines = [{
-        "type": "LineString",
-        "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
-    }, {
-        "type": "LineString",
-        "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
-    }];
-
-    var myStyle = {
-        "color": "#ff7800",
-        "weight": 5,
-        "opacity": 0.65
-    };
-
-    L.geoJSON(myLines, {
-        style: myStyle
-    }).addTo(map);
-
-    var states = [{
-        "type": "Feature",
-        "properties": {"party": "Republican"},
-        "geometry": {
-            "type": "Polygon",
-            "coordinates": [[
-                [-104.05, 48.99],
-                [-97.22,  48.98],
-                [-96.58,  45.94],
-                [-104.03, 45.94],
-                [-104.05, 48.99]
-            ]]
-        }
-    }, {
-        "type": "Feature",
-        "properties": {"party": "Democrat"},
-        "geometry": {
-            "type": "Polygon",
-            "coordinates": [[
-                [-109.05, 41.00],
-                [-102.06, 40.99],
-                [-102.03, 36.99],
-                [-109.04, 36.99],
-                [-109.05, 41.00]
-            ]]
-        }
-    }];
-
-    L.geoJSON(states, {
-    style: function(feature) {
-        switch (feature.properties.party) {
-            case 'Republican': return {color: "#ff0000"};
-            case 'Democrat':   return {color: "#0000ff"};
-        }
-    }
-    }).addTo(map);
-
-
-    .then(function(json){
-
-        var geojsonMarkerOptions = {
-        radius: 8,
-        fillColor: "#ff7800",
-        color: "#000",
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.8
-        };
-    });
-
-    L.geoJson(json, {
-                pointToLayer: function (feature, latlng){
-                    return L.circleMarker(latlng, geojsonMarkerOptions);
-                }
-            }).addTo(map);
-   
-
-    // a class.. A class for drawing circle overlays on a map
-    var circle = L.circle([39.75621, -104.99404], {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
-        radius: 500
-        }).addTo(mymap);
-
-    marker.bindPopup("<strong>Hello world!</strong><br />I am a popup.").openPopup();
-    circle.bindPopup("I am a circle.");
-    polygon.bindPopup("I am a polygon.");
-
-
-    //Used to open popups in certain places of the map
-    var popup = L.popup()
-    .setLatLng([39.75621, -104.99404])
-    .setContent("I am a standalone popup.")
-    .openOn(mymap);
-
-    var popup = L.popup();
-
-//event listeners when clicking on the map
-    function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(mymap);
+/* Map of GeoJSON data from MegaCities.geojson */
+// Function to instantiate the Leaflet map
+function createMap(){
+    // The map is already created globally as mymap
 }
 
-    mymap.on('click', onMapClick);
+function onEachFeature(feature, layer) {
+    //no property named popupContent; instead, create html string with all properties
+    var popupContent = "";
+    if (feature.properties) {
+        //loop to add feature property names and values to html string
+        for (var property in feature.properties){
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+        }
+        layer.bindPopup(popupContent);
+    };
+};
+
+//function to retrieve the data and place it on the map
+function getData(){
+    //load the data
+    fetch("data/MegaCities.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(json).addTo(map);
+        })
+};
+
+document.addEventListener('DOMContentLoaded',createMap)
+
+// function onEachFeature(feature, layer) {
+//     // does this feature have a property named popupContent?
+//     if (feature.properties && feature.properties.popupContent) {
+//         layer.bindPopup(feature.properties.popupContent);
+//     }
+// }
+
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+};
+
+L.geoJSON(geojsonFeature, {
+    onEachFeature: onEachFeature
+}).addTo(map);
+
+var myLines = [{
+    "type": "LineString",
+    "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
+}, {
+    "type": "LineString",
+    "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
+}];
+
+var myStyle = {
+    "color": "#ff7800",
+    "weight": 5,
+    "opacity": 0.65
+};
+
+L.geoJSON(myLines, {
+    style: myStyle
+}).addTo(map);
+
+var states = [{
+    "type": "Feature",
+    "properties": {"party": "Republican"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-104.05, 48.99],
+            [-97.22,  48.98],
+            [-96.58,  45.94],
+            [-104.03, 45.94],
+            [-104.05, 48.99]
+        ]]
+    }
+}, {
+    "type": "Feature",
+    "properties": {"party": "Democrat"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-109.05, 41.00],
+            [-102.06, 40.99],
+            [-102.03, 36.99],
+            [-109.04, 36.99],
+            [-109.05, 41.00]
+        ]]
+    }
+}];
+
+L.geoJSON(states, {
+style: function(feature) {
+    switch (feature.properties.party) {
+        case 'Republican': return {color: "#ff0000"};
+        case 'Democrat':   return {color: "#0000ff"};
+    }
+}
+}).addTo(map);
+
+.then(function(json){
+
+    var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+    };
+});
+
+L.geoJson(json, {
+            pointToLayer: function (feature, latlng){
+                return L.circleMarker(latlng, geojsonMarkerOptions);
+            }
+        }).addTo(map);
+
+// a class.. A class for drawing circle overlays on a map
+var circle = L.circle([39.75621, -104.99404], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+    }).addTo(mymap);
+
+marker.bindPopup("<strong>Hello world!</strong><br />I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+//Used to open popups in certain places of the map
+var popup = L.popup()
+.setLatLng([39.75621, -104.99404])
+.setContent("I am a standalone popup.")
+.openOn(mymap);
+
+var popup = L.popup();
+
+//event listeners when clicking on the map
+function onMapClick(e) {
+popup
+    .setLatLng(e.latlng)
+    .setContent("You clicked the map at " + e.latlng.toString())
+    .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
